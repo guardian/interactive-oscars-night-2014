ba8f7ca1ae854ea9b3956eb45f5feb29d49907f4
feilding <feilding.cage@gmail.com>
2015-02-19 17:20:46 +0000

    adds apps styling

--- src/js/modules/pageInfo.js
+++ src/js/modules/pageInfo.js
@@ -6,18 +6,22 @@ define([], function() {
 
         //adds styles the header
         var headerBg = document.getElementsByClassName('content__head');
+        if(headerBg.length == 0){
+            headerBG = document.getElementsByClassName('article__header');
+        }
         for(var i = 0; i < headerBg.length; i ++){
             var el = headerBg[i];
             el.className += ' guOscarsHeader';
         }
 
         var oscarBG = document.getElementsByClassName('content__main-column');
+
         for(var i = 0; i < oscarBG.length; i ++){
             if(i == 0){
                 var newNode = document.createElement('div'); 
                 newNode.className = 'guOscarStatue'; 
 
-                var el = oscarBG[0];
+                var el = oscarBG[i];
                 el.appendChild(newNode);
             }
         }
@@ -27,6 +31,9 @@ define([], function() {
 
         //adds styles content
         var contentBg = document.getElementsByClassName('content');
+        if(contentBg.length == 0){
+            contentBg = document.getElementsByClassName('article__body');
+        }
         for(var i = 0; i < contentBg.length; i ++){
             var el = contentBg[i];
             el.className += ' guOscarsContent';
